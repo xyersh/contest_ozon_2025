0a71a2796348390416a8a871d50a61c8a184c96e
Alex Yersh <xyersh@gmail.com>
2025-08-15 17:37:19 +0300

    правки в наполении карт Print() - работает годно

--- task_3/main.go
+++ task_3/main.go
@@ -32,21 +32,24 @@ func main() {
 	)
 
 	fmt.Fscan(reader, &num_of_levels)
-	fmt.Printf("num_of_levels = %d\n", num_of_levels)
+	// fmt.Printf("num_of_levels = %d\n", num_of_levels)
 	for i := 0; i < num_of_levels; i++ {
-		fmt.Fscan(reader, &x_size, &y_size)
-		fmt.Printf("x_size = %d  y_size = %d\n", x_size, y_size)
+		fmt.Fscan(reader, &y_size, &x_size)
+		reader.ReadString('\n')
+		// fmt.Printf("x_size = %d  y_size = %d\n", x_size, y_size)
 
 		mtx := NewMatrix(x_size, y_size)
-		reader.ReadString('\n')
 
+		// fmt.Println(str)
+		// reader.ReadString('\n')
 		for j := 0; j < int(y_size); j++ {
 			line, _ := reader.ReadString('\n')
 			line = strings.TrimRight(line, "\n")
 			// fmt.Printf("%s", line)
 			mtx.field[j] = []rune(line)
-			fmt.Println(string(mtx.field[j]))
+			// fmt.Println(string(mtx.field[j]))
 		}
+
 		mtx.Print()
 
 	}
@@ -79,27 +82,15 @@ func NewMatrix(x_size, y_size Dimension) *Matrix {
 }
 
 func (m *Matrix) Print() {
-	fmt.Println("m.Print() - STARTS")
-	fmt.Printf("x_size = %d\ny_size = %d\n", m.x_size, m.x_size)
+	// fmt.Println("m.Print() - STARTS")
+	// fmt.Printf("x_size = %d\ny_size = %d\n", m.x_size, m.y_size)
 
 	var bldr strings.Builder
-	bldr.Grow(int(m.x_size+2) * int(m.y_size+2))
-	// mtx_wdth := len(matrix[0])
-
-	// //верхняя граница
-	// bldr.WriteRune('+')
-	// bldr.WriteString(strings.Repeat("-", int(m.x_size)))
-	// bldr.WriteString("+\n")
+	bldr.Grow(int(m.x_size) * int(m.y_size))
 
 	for i := 0; i < int(m.y_size); i++ {
-		// bldr.WriteString("|" + string(m.field[i]) + "|\n")
 		bldr.WriteString(string(m.field[i]) + "\n")
 	}
-
-	// // нижняя граница
-	// bldr.WriteRune('+')
-	// bldr.WriteString(strings.Repeat("-", int(m.x_size)))
-	// bldr.WriteString("+\n")
 
 	fmt.Println(bldr.String())
 
